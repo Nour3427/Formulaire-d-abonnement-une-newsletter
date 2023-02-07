22d512f007a579afbee470ad92056745db1c30fb
Nour3427 <nourelyakine.moumene@gmail.com>
2023-02-07 20:59:57 +0100

    ajout de fichier config.exemple.php

--- config.example.php
+++ config.example.php
@@ -1,7 +1,7 @@
 
 <?php 
-const DB_HOST = 'localhost';
-const DB_NAME = 'newsletter';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
+const DB_HOST = '1212.12.212';
+const DB_NAME = 'DBName_Example';
+const DB_USER = 'ADMIN';
+const DB_PASSWORD = 'PSWRD';
 
--- newsletter.sql
+++ newsletter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 07, 2023 at 05:59 PM
+-- Generation Time: Feb 07, 2023 at 07:31 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -87,7 +87,7 @@ ALTER TABLE `origines`
 -- AUTO_INCREMENT for table `subscribers`
 --
 ALTER TABLE `subscribers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
